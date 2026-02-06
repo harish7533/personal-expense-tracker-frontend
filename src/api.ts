064608c69f68b0fdb176cf8e85f10a946de0252c
harish7533 <harishbalamurugan.2002@gmail.com>
2026-02-06 08:04:39 +0530

    Updated the api.ts code to production based backend URL

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4590/api", // backend port fixed
+  baseURL: import.meta.env.VITE_API_URL, // backend port fixed
 });
 
 /* Attach JWT token automatically */
